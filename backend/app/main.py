e9bd1780cef5d88016651fbcbc13b8a7d8aa7ed6
farhan-navas <farhanmnavas@gmail.com>
2025-08-01 00:19:46 +0800

    update backend endpoints

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,6 +2,9 @@ from pydantic import BaseModel
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
+from azure.identity import DefaultAzureCredential
+from azure.ai.projects import AIProjectClient
+
 app = FastAPI()
 
 origins = [
@@ -16,9 +19,28 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-class PayloadType(BaseModel):
+project = AIProjectClient(
+    endpoint="https://hackathon-group4-resource.services.ai.azure.com/api/projects/hackathon-group4",
+    credential=DefaultAzureCredential()
+)
+
+class ChatPayloadType(BaseModel):
     content: str
+
+class FormPayloadType(BaseModel):
+    industry: str
+    region: str
+    country: str
 
 @app.get("/")
 def get_root_data():
-    return { "Try try" }
+    return { 'Welcome to Market Scanner Backend!' }
+
+@app.post('/api/scanner-chat')
+def post_chat_data(chat_payload: ChatPayloadType):
+    content = chat_payload.content
+
+@app.post("/api/scanner-form")
+def post_form_data(form_payload: FormPayloadType):
+    industry, region, country = form_payload.industry, form_payload.region, form_payload.country
+
